c70c5347e403102cab762024eb7b616f23f5ec56
Mars Tan <davidtjx424@live.com>
2016-09-26 21:27:55 +0800

    First release of DateCalculator

--- apps/DateCalculator/src/cc/isotopestudio/datecalculator/DateCal.java
+++ apps/DateCalculator/src/cc/isotopestudio/datecalculator/DateCal.java
@@ -4,4 +4,81 @@ package cc.isotopestudio.datecalculator;/*
  */
 
 public class DateCal {
+
+    private static boolean ifYear(int a) {
+        return a % 100 == 0 ? a % 400 == 0 : a % 4 == 0;
+    }
+
+    public static int getMonthDays(int year, int a) {
+        if (a == 2) {
+            return ifYear(year) ? 29 : 28;
+        } else if (a <= 7 && a > 0) {
+            return a % 2 == 0 ? 30 : 31;
+        } else if (a >= 8 && a <= 12) {
+            return a % 2 == 0 ? 31 : 30;
+        } else
+            return 0;
+    }
+
+    public static boolean ifLarger(int aYear, int aMonth, int aDay, int bYear, int bMonth, int bDay) {
+        if (bYear > aYear)
+            return true;
+        if (aYear > bYear)
+            return false;
+
+        if (bMonth > aMonth)
+            return true;
+        if (aMonth > bMonth)
+            return false;
+
+        if (bDay > aDay)
+            return true;
+        if (aDay > bDay)
+            return false;
+
+        return true;
+
+    }
+
+    public static long cal(int aYear, int aMonth, int aDay, int bYear, int bMonth, int bDay) {
+
+        long resultD = 0;
+
+
+        if (bYear != aYear) {
+            // 中间年份 日期总数
+            int middleD = 0;
+            for (int i = aYear + 1; i < bYear; i++)
+                if (ifYear(i))
+                    middleD++;
+            middleD += (bYear - aYear - 1) * 365;
+            resultD += middleD;
+
+            // 今年1月1日到今天的日期数
+            int lastD = 0;
+            for (int i = 1; i < bMonth; i++) {
+                lastD += getMonthDays(bYear, i);
+            }
+            lastD += bDay;
+            resultD += lastD;
+
+            // 出生日期到12月31日天数
+            int firstD = 0;
+            for (int i = aMonth + 1; i <= 12; i++)
+                firstD += getMonthDays(bYear, i);
+            firstD += getMonthDays(bYear, aMonth) - aDay;
+            resultD += firstD;
+        } else {
+            if (aMonth != bMonth) {
+                resultD += getMonthDays(aYear, aMonth) - aDay;
+                for (int i = aMonth + 1; i < bMonth; i++) {
+                    resultD += getMonthDays(aYear, i);
+                }
+                resultD += bDay;
+            } else
+                resultD = bDay - aDay;
+        }
+
+        return resultD;
+    }
 }
--- apps/DateCalculator/src/cc/isotopestudio/datecalculator/DateGUI.java
+++ apps/DateCalculator/src/cc/isotopestudio/datecalculator/DateGUI.java
@@ -3,5 +3,156 @@ package cc.isotopestudio.datecalculator;/*
  * Copyright ISOTOPE Studio
  */
 
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class DateGUI {
+    private JSpinner aMonS;
+    private JPanel panel1;
+    private JSpinner aDayS;
+    private JSpinner aYearS;
+    private JSpinner bMonS;
+    private JSpinner bDayS;
+    private JSpinner bYearS;
+    private JTextField resultBox;
+    private JButton infobtn;
+    private JButton exitbtn;
+    private JPanel panel;
+
+    private static JFrame frame;
+
+    public static void main(String[] args) {
+        frame = new JFrame("DateGUI");
+        frame.setContentPane(new DateGUI().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+
+    public DateGUI() {
+        GregorianCalendar g = new GregorianCalendar();
+        aYearS.setValue(g.get(Calendar.YEAR));
+        aMonS.setValue(g.get(Calendar.MONTH) + 1);
+        aDayS.setValue(g.get(Calendar.DATE));
+        bYearS.setValue(g.get(Calendar.YEAR) + 1);
+        bMonS.setValue(g.get(Calendar.MONTH) + 1);
+        bDayS.setValue(g.get(Calendar.DATE));
+        update();
+        infobtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Toolkit.getDefaultToolkit().beep();
+                JOptionPane.showMessageDialog(frame, "Date Calculator 1.0.0\nISOTOPE Studio, Mars\n2016.9.26",
+                        "Info",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        exitbtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int temp = (int) aMonS.getValue();
+                if (temp <= 0)
+                    aMonS.setValue(1);
+                else if (temp > 12)
+                    aMonS.setValue(12);
+                frame.dispose();
+                System.exit(0);
+            }
+        });
+
+        aYearS.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                vaildateA();
+            }
+        });
+        aDayS.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                vaildateA();
+            }
+        });
+        aMonS.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                vaildateA();
+            }
+        });
+        bYearS.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                vaildateB();
+            }
+        });
+        bDayS.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                vaildateB();
+            }
+        });
+        bMonS.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                vaildateB();
+            }
+        });
+    }
+
+    private void vaildateA() {
+        int temp = (int) aYearS.getValue();
+        if (temp <= 0)
+            aYearS.setValue(1);
+        else if (temp > 2500)
+            aYearS.setValue(2500);
+
+        temp = (int) aMonS.getValue();
+        if (temp <= 0)
+            aMonS.setValue(1);
+        else if (temp > 12)
+            aMonS.setValue(12);
+
+        temp = (int) aDayS.getValue();
+        if (temp <= 0)
+            aDayS.setValue(1);
+        else if (temp > DateCal.getMonthDays((int) aYearS.getValue(), (int) aMonS.getValue()))
+            aDayS.setValue(DateCal.getMonthDays((int) aYearS.getValue(), (int) aMonS.getValue()));
+        update();
+    }
+
+    private void vaildateB() {
+        int temp = (int) bYearS.getValue();
+        if (temp <= 0)
+            bYearS.setValue(1);
+        else if (temp > 2500)
+            bYearS.setValue(2500);
+
+        temp = (int) bMonS.getValue();
+        if (temp <= 0)
+            bMonS.setValue(1);
+        else if (temp > 12)
+            bMonS.setValue(12);
+
+        temp = (int) bDayS.getValue();
+        if (temp <= 0)
+            bDayS.setValue(1);
+        else if (temp > DateCal.getMonthDays((int) bYearS.getValue(), (int) bMonS.getValue()))
+            bDayS.setValue(DateCal.getMonthDays((int) bYearS.getValue(), (int) bMonS.getValue()));
+        update();
+    }
+
+    private void update() {
+        if (DateCal.ifLarger((int) aYearS.getValue(), (int) aMonS.getValue(), (int) aDayS.getValue(),
+                (int) bYearS.getValue(), (int) bMonS.getValue(), (int) bDayS.getValue()))
+            resultBox.setText("" + DateCal.cal((int) aYearS.getValue(), (int) aMonS.getValue(), (int) aDayS.getValue(),
+                    (int) bYearS.getValue(), (int) bMonS.getValue(), (int) bDayS.getValue()));
+        else
+            resultBox.setText("Error!");
+    }
 }
