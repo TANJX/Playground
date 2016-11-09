2b1ddaa14b9c981c3bb6e0d46fd42937f78643e6
Mars Tan <davidtjx424@live.com>
2016-11-09 22:18:18 +0800

    Update TimeGUI

--- src/app/timeCalculator/TimeGUI.java
+++ src/app/timeCalculator/TimeGUI.java
@@ -31,29 +31,71 @@ public class TimeGUI {
     private JSlider time1MinuteSlider;
     private JSpinner time1MinuteSpinner;
 
+    private static final String VERSION = "0.1.1";
+
     public TimeGUI() {
 
         time1HourSpinner.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
+                validate(0);
+                sync(0, 0);
+                updateResult();
+            }
+        });
+        time1HourSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                validate(0);
+                sync(0, 1);
                 updateResult();
             }
         });
         time1MinuteSpinner.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
+                validate(1);
+                sync(1, 0);
+                updateResult();
+            }
+        });
+        time1MinuteSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                validate(1);
+                sync(1, 1);
                 updateResult();
             }
         });
         time2HourSpinner.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
+                validate(2);
+                sync(2, 0);
+                updateResult();
+            }
+        });
+        time2HourSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                validate(2);
+                sync(2, 1);
                 updateResult();
             }
         });
         time2MinuteSpinner.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
+                validate(3);
+                sync(3, 0);
+                updateResult();
+            }
+        });
+        time2MinuteSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                validate(3);
+                sync(3, 1);
                 updateResult();
             }
         });
@@ -61,7 +103,7 @@ public class TimeGUI {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Toolkit.getDefaultToolkit().beep();
-                JOptionPane.showMessageDialog(frame, "Time Calculator 0.1.0\nISOTOPE Studio, Mars\n2016.11.8",
+                JOptionPane.showMessageDialog(frame, "Time Calculator " + VERSION + "\nISOTOPE Studio, Mars\n2016.11.9",
                         "Info",
                         JOptionPane.INFORMATION_MESSAGE);
             }
@@ -83,8 +125,83 @@ public class TimeGUI {
         resultField.setText(Time.getInterval(h1, m1, h2, m2));
     }
 
+    /**
+     * @param p 0, 1, 2, or 3 (time1hour, time1minute, time2hour, time2minute)
+     */
+    private void validate(int p) {
+        switch (p) {
+            case (0):
+                time1HourSpinner.setValue(getValidatedValue(0, (int) time1HourSpinner.getValue()));
+                return;
+            case (1):
+                time1MinuteSpinner.setValue(getValidatedValue(1, (int) time1MinuteSpinner.getValue()));
+                return;
+            case (2):
+                time2HourSpinner.setValue(getValidatedValue(0, (int) time2HourSpinner.getValue()));
+                return;
+            case (3):
+                time2MinuteSpinner.setValue(getValidatedValue(1, (int) time2MinuteSpinner.getValue()));
+        }
+    }
+
+    /**
+     * @param type  0 or 1 (hour or minute)
+     * @param value value need to be adjusted
+     * @return validated value
+     */
+    private int getValidatedValue(int type, int value) {
+        if (value < 0)
+            return 0;
+        if (type == 0 && value > 24)
+            return 24;
+        if (type == 1 && value > 60)
+            return 60;
+        return value;
+
+    }
+
+    /**
+     * @param p    0, 1, 2, or 3 (time1hour, time1minute, time2hour, time2minute)
+     * @param type 0 or 1 (spinner or slider)
+     */
+    private void sync(int p, int type) {
+        switch (p) {
+            case (0):
+                if (type == 0) {
+                    time1HourSlider.setValue((Integer) time1HourSpinner.getValue());
+                }
+                if (type == 1) {
+                    time1HourSpinner.setValue(time1HourSlider.getValue());
+                }
+                return;
+            case (1):
+                if (type == 0) {
+                    time1MinuteSlider.setValue((Integer) time1MinuteSpinner.getValue());
+                }
+                if (type == 1) {
+                    time1MinuteSpinner.setValue(time1MinuteSlider.getValue());
+                }
+                return;
+            case (2):
+                if (type == 0) {
+                    time2HourSlider.setValue((Integer) time2HourSpinner.getValue());
+                }
+                if (type == 1) {
+                    time2HourSpinner.setValue(time2HourSlider.getValue());
+                }
+                return;
+            case (3):
+                if (type == 0) {
+                    time2MinuteSlider.setValue((Integer) time2MinuteSpinner.getValue());
+                }
+                if (type == 1) {
+                    time2MinuteSpinner.setValue(time2MinuteSlider.getValue());
+                }
+        }
+    }
+
     public static void main(String[] args) {
-        frame = new JFrame("Time Calculator 0.1.0");
+        frame = new JFrame("Time Calculator " + VERSION);
         frame.setContentPane(new TimeGUI().panel);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
